b4da6ca4c19eca95c960de14fe81df10705cf213
Federico Scafoglieri <scafoglierifederico@gmail.com>
2025-10-29 17:22:25 +0100

    fixing hybea

---
+++ src/alignment_models/methods/hybea/__init__.py
@@ -0,0 +1,15 @@
+from .runtime import (
+    apply_settings,
+    dataset_spec,
+    path_for_KG,
+    runtime,
+    topk_inputsize1_inputsize2,
+)
+
+__all__ = [
+    "runtime",
+    "apply_settings",
+    "path_for_KG",
+    "dataset_spec",
+    "topk_inputsize1_inputsize2",
+]
