42163d8eca988f229da8715069ceb3b13b9f8c4d
Federico Scafoglieri <scafoglierifederico@gmail.com>
2025-11-06 10:30:26 +0100

    Revert "fixing bert_int"
    This reverts commit 766daf53bcd8809078a5df86db43376a7b9bfab7.

--- hybea/src/generate_names/OLDParam.py
+++
@@ -1,7 +0,0 @@
-# from config.config import DATASET, BASE_DIR
-from src.alignment_models.methods.hybea import legacy_config as cfg
-
-# Choose the dataset that will be used to extract the entity names
-# Choose between D_W_15K_V1, D_W_15K_V2, SRPRS_D_W_15K_V1, SRPRS_D_W_15K_V2, BBC_DB
-DATASET = cfg.DATASET
-BASE_DIR = cfg.BASE_DIR
--- hybea/src/generate_names/generate_names.py
+++
@@ -1,7 +0,0 @@
-from hybea.src.generate_names.name_analysis import run_name_analysis
-from hybea.src.generate_names.prioritize_names import run_prioritize
-
-
-def run_generate_names():
-    run_name_analysis()
-    run_prioritize()
