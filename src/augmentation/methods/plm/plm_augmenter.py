1b43e65c7826d000dbccb2d5e057e523f1cdaf5e
Federico Scafoglieri <scafoglierifederico@gmail.com>
2025-11-10 17:20:11 +0100

    Implementation graph visualization

--- src/augmentation/methods/plm/plm_augmenter.py
+++ src/augmentation/methods/plm/plm_augmenter.py
@@ -17,4 +17,16 @@ class PLMAugmenter(AugmentationMethod):
         super().__init__(config)
 
     def augment(self, dataset: Dataset) -> Dataset:
-        pass
+        # TODO: Implement PLM-based augmentation
+
+        # Prendi un nodo set random
+        # Crea due nuovi nodi con due id uno coerente con il primo grafo e uno coerente con il seconod grafo
+        # Espandi i due nodi con i rispetti attribuiti interpolazione etc.
+        # Richiama Augmentation in maniera ricorsiva su nodo vicino se anche questo set/allineato
+        # Altrimenti se non è un nodo set fai una semplice augmentation
+        # Richiama nuovamente fino ad arrivare una profondità massima "d" data
+        # Se il vicino di un nodo non set è un nodo set fera quelli da espandere allora devi espandere seguendo quanto fatto all`inizio
+        # se non ci sono più vicini allora verifica che tutti quelli che dovevano essere espansi sono stati espansi in quanto il grafo potrebbe non essere connesso.
+
+
+        return dataset
